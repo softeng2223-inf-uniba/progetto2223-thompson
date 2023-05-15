761e79d21949f3983fa99a55692942f22dc379a6
Erusso26 <emanuelerusso228@gmail.com>
2023-05-15 17:42:29 +0200

    Aggiunta classe delle difficoltà e relativi metodi per impostarla

--- src/main/java/it/uniba/app/Partita.java
+++ src/main/java/it/uniba/app/Partita.java
@@ -3,6 +3,7 @@ package it.uniba.app;
 import it.uniba.app.grid.Grid;
 import it.uniba.app.parser.Parser;
 import it.uniba.app.type.Command;
+import it.uniba.app.type.Difficulty;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -10,13 +11,22 @@ import java.util.Scanner;
 /**
  * Class that interprets commands.
  */
-
 public class Partita {
 
     /**
      * Grid of the current match.
      */
     private Grid grid;
+
+    /**
+     * Difficulty of the current match.
+     */
+    private Difficulty gameDifficulty ;
+
+    /**
+     * Flag that indicates if a game is in progress.
+     */
+    private boolean isInGame;
 
     /**
      * Method for starting a game.
@@ -26,6 +36,7 @@ public class Partita {
         System.out.println("*   Battleship  -  2022/2023   *");
         System.out.println("================================");
         System.out.println();
+        gameDifficulty = Difficulty.MEDIUM; //default difficulty
         Scanner scanner = new Scanner(System.in, "UTF-8");
         while (scanner.hasNextLine()) {
             executeCommand(Parser.parse(scanner.nextLine()));
@@ -40,6 +51,12 @@ public class Partita {
             System.out.println("Comando non riconosciuto");
         } else if (command.containsKey(Command.EXIT) && command.get(Command.EXIT).isEmpty()) {
             closeGame();
+        } else if (command.containsKey(Command.EASY) && command.get(Command.EASY).isEmpty()) {
+            setDifficulty(command);
+        } else if (command.containsKey(Command.MEDIUM) && command.get(Command.MEDIUM).isEmpty()) {
+            setDifficulty(command);
+        } else if (command.containsKey(Command.HARD) && command.get(Command.HARD).isEmpty()) {
+            setDifficulty(command);
         } else {
             System.out.println("Comando non valido");
         }
@@ -56,6 +73,24 @@ public class Partita {
             if (p.containsKey(Command.YES)) {
                 Runtime.getRuntime().exit(0);
             }
+        }
+    }
+
+    /**
+     * Method for change difficulty
+     */
+    private void setDifficulty(final Command.command) {
+        if (isInGame) {
+            System.out.println("E' in corso una partita, non puoi cambiare difficoltà");
+        } else {
+            if (command == Command.EASY) {
+                gameDifficulty = Difficulty.EASY;
+            } else if (command == Command.MEDIUM) {
+                gameDifficulty = Difficulty.MEDIUM;
+            } else if (command == Command.HARD) {
+                gameDifficulty = Difficulty.HARD;
+            }
+            System.out.println("OK");
         }
     }
 }
---
+++ src/main/java/it/uniba/app/type/Difficulty.java
@@ -0,0 +1,38 @@
+package it.uniba.app.type;
+
+/**
+ *Class containing difficulty and number of attempts.
+ */
+public enum Difficulty {
+    /**
+     * Easy difficulty with 50 tries.
+     */
+    EASY(50),
+    /**
+     * Medium difficulty with 30 tries.
+     */
+    MEDIUM(30),
+    /**
+     * Hard difficulty with 10 tries.
+     */
+    HARD(10);
+
+    /**
+     * Contains number of tries.
+     */
+    private final int tries;
+
+    /**
+     * Constructor of the difficulty class.
+     */
+    Difficulty(final int valTries) {
+        this.tries = valTries;
+    }
+
+    /**
+     * Returns number of tries.
+     */
+    public int getTries() {
+        return tries;
+    }
+}
