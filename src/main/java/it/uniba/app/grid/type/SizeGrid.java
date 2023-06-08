4fe7db6e1496570bdc785ba3a7a75aa41f6da4aa
Erusso26 <emanuelerusso228@gmail.com>
2023-06-08 13:07:32 +0200

    Aggiunta Javadoc nel file SizeGrid

--- src/main/java/it/uniba/app/grid/type/SizeGrid.java
+++ src/main/java/it/uniba/app/grid/type/SizeGrid.java
@@ -1,7 +1,7 @@
 package it.uniba.app.grid.type;
 
 /**
- * Enum class to describe the dimensions of the grid.
+ * Represents the size of a grid.
  */
 public enum SizeGrid {
     STANDARD(10),
@@ -12,14 +12,29 @@ public enum SizeGrid {
 
     private static SizeGrid currentSize = SizeGrid.STANDARD;
 
+    /**
+     * Constructs a SizeGrid object with the specified size.
+     *
+     * @param valSize the size value of the grid
+     */
     SizeGrid(final int valSize) {
         this.size = valSize;
     }
 
+    /**
+     * Returns the current size of the grid.
+     *
+     * @return the current size of the grid
+     */
     public static int getSize() {
         return currentSize.size;
     }
 
+    /**
+     * Sets the size of the grid to the specified value.
+     *
+     * @param valSize the new size of the grid
+     */
     public static void setSize(final SizeGrid valSize) {
         currentSize = valSize;
     }
