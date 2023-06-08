302590ce56173af6a737ee107469bd80d6f8f014
lmatera19 <lorenzo.matera.00@gmail.com>
2023-06-08 16:00:50 +0200

    Modifica classe Column

--- src/main/java/it/uniba/app/grid/type/Column.java
+++ src/main/java/it/uniba/app/grid/type/Column.java
@@ -4,16 +4,18 @@ package it.uniba.app.grid.type;
  * Enum class to describe coordinate columns.
  */
 public enum Column {
-    A, B, C, D, E, F, G, H, I, J;
+    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
 
     /**
-     * This array contains column values, is used as a buffer so as not to recreate it
+     * This array contains column values, is used as a buffer so as not to recreate
+     * it
      * each time.
      */
     private static final Column[] VALUES = Column.values();
 
     /**
      * Method to convert integer to column type.
+     *
      * @param value integer
      * @return value or column
      */
