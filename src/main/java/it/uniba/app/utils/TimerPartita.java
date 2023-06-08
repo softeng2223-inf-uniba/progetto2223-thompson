30f95bb63c477e519b05c32dcf0c76ff4799a935
RobertoRotunno <rotunno.web@gmail.com>
2023-06-08 14:34:58 +0200

    implementata la classe del timer

---
+++ src/main/java/it/uniba/app/utils/TimerPartita.java
@@ -0,0 +1,94 @@
+package it.uniba.app.utils;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Utility class for managing game timers.
+ */
+public class TimerPartita {
+    private static final long DEFAULT_TIME = 10;
+    private static long maxTime = TimeUnit.MILLISECONDS.convert(DEFAULT_TIME, TimeUnit.MINUTES);
+    private static Timer timer;
+    private static boolean isRunning;
+    private long startTime;
+
+    /**
+     * Checks if the timer is currently running.
+     *
+     * @return true if the timer is running, false otherwise
+     */
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    /**
+     * Sets the running state of the timer.
+     *
+     * @param valIsRunning true to set the timer as running, false otherwise
+     */
+    public static void setRunning(final boolean valIsRunning) {
+        isRunning = valIsRunning;
+    }
+
+    /**
+     * Starts the game timer.
+     */
+    public void startGame() {
+        timer = new Timer();
+        startTime = System.currentTimeMillis();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                stopTimer();
+            }
+        };
+        timer.schedule(task, maxTime);
+        System.out.println("Il timer di " + getMaxTime() + " minuti. E' ");
+        isRunning = true;
+    }
+
+    /**
+     * Stops the game timer if it is running.
+     */
+    public static void stopTimer() {
+        if (isRunning) {
+            System.out.println("Il timer è scaduto. La partita è finita!");
+            isRunning = false;
+        }
+        timer.cancel();
+    }
+
+    /**
+     * Prints the current time and remaining time based on the maximum time.
+     */
+    public void printCurrentTime() {
+        long currentTime = TimeUnit.MINUTES.convert(getCurrentTimeMillis(), TimeUnit.MILLISECONDS);
+        long maxMinute = TimeUnit.MINUTES.convert(maxTime, TimeUnit.MILLISECONDS);
+        System.out.print("Sono passati " + currentTime + ", mancano " + (maxMinute - currentTime));
+    }
+
+    /**
+     * Returns the maximum time in minutes for the game timer.
+     *
+     * @return the maximum time in minutes
+     */
+    public static long getMaxTime() {
+        return TimeUnit.MINUTES.convert(maxTime, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * Sets the maximum time in minutes for the game timer.
+     *
+     * @param minutes the maximum time in minutes
+     */
+    public static void setMaxTime(final int minutes) {
+        TimerPartita.maxTime = TimeUnit.MILLISECONDS.convert(minutes, TimeUnit.MINUTES);
+    }
+
+    private long getCurrentTimeMillis() {
+        return System.currentTimeMillis() - startTime;
+    }
+}
