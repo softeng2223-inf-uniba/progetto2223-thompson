5497ee15a28ade0720fe4911a953eff7181fae10
LeoMastro8 <leomastronardi10@gmail.com>
2023-06-09 10:11:46 +0200

    Aggiunto javadoc per la classe GridPrinter

--- src/main/java/it/uniba/app/grid/GridPrinter.java
+++ src/main/java/it/uniba/app/grid/GridPrinter.java
@@ -6,15 +6,47 @@ import it.uniba.app.ship.Ship;
 
 public class GridPrinter {
 
+    /**
+     * The formatter for column values.
+     */
     private static String formatterCol = "";
+
+    /**
+     * The formatter for row separators.
+     */
     private static String formatterRow = "";
+
+    /**
+     * The formatter for empty cells.
+     */
     private static String formatterVoid = "";
+
+    /**
+     * The formatter for space characters.
+     */
     private static String formatterSpace = "";
 
-    private static final int MAX_SIZE_SMALL=10;
-    private static final int MAX_SIZE_MEDIUM=18;
-    private static final int MAX_SIZE_LARGE=26;
+    /**
+     * The maximum size for a small grid.
+     */
+    private static final int MAX_SIZE_SMALL = 10;
 
+    /**
+     * The maximum size for a medium grid.
+     */
+    private static final int MAX_SIZE_MEDIUM = 18;
+
+    /**
+     * The maximum size for a large grid.
+     */
+    private static final int MAX_SIZE_LARGE = 26;
+
+    /**
+     * Method to display the grid with the ships.
+     * Format the grid display according to its size
+     * 
+     * @param grid The grid to be printed.
+     */
     public static void printGrid(Grid grid) {
         int size = grid.getSize();
         GridPrinter.setFormatters(size);
@@ -37,7 +69,9 @@ public class GridPrinter {
             System.out.print(String.format(" %2d |", (i + 1)));
             for (int j = 0; j < size; j++) {
                 System.out.print(
-                        grid.getGrid()[i][j].getShip() != null ? formatterSpace+grid.getGrid()[i][j].getShip().colorShip() : formatterVoid);
+                        grid.getGrid()[i][j].getShip() != null
+                                ? formatterSpace + grid.getGrid()[i][j].getShip().colorShip()
+                                : formatterVoid);
                 System.out.print(formatterSpace);
                 System.out.print("|");
             }
@@ -49,9 +83,11 @@ public class GridPrinter {
      * Prints the current grid with the state of each cell.
      * Display HIT cells in red and MISS cells in white.
      * Format the grid display according to its size
+     * 
+     * @param grid The grid to be printed.
      */
     public static final void printCurrentGrid(Grid grid) {
-        int size=grid.getSize();
+        int size = grid.getSize();
         GridPrinter.setFormatters(size);
         System.out.println();
         System.out.print("  |");
@@ -82,22 +118,27 @@ public class GridPrinter {
         }
     }
 
+    /**
+     * Sets the formatters based on the given grid size.
+     *
+     * @param size The size of the grid.
+     */
     public static void setFormatters(int size) {
-        if (size>0 && size<=MAX_SIZE_SMALL) {
-                formatterCol = "   %s   |";
-                formatterRow = "-------+";
-                formatterVoid= "    ";
-                formatterSpace = "   ";
-        } else if (size>MAX_SIZE_SMALL && size<=MAX_SIZE_MEDIUM) {
-                formatterCol = "  %s  |";
-                formatterRow = "-----+";
-                formatterVoid = "   ";
-                formatterSpace = "  ";
-        }else if (size>MAX_SIZE_MEDIUM && size<=MAX_SIZE_LARGE) {
-                formatterCol = " %s |";
-                formatterRow = "---+";
-                formatterVoid = "  ";
-                formatterSpace = " ";
+        if (size > 0 && size <= MAX_SIZE_SMALL) {
+            formatterCol = "   %s   |";
+            formatterRow = "-------+";
+            formatterVoid = "    ";
+            formatterSpace = "   ";
+        } else if (size > MAX_SIZE_SMALL && size <= MAX_SIZE_MEDIUM) {
+            formatterCol = "  %s  |";
+            formatterRow = "-----+";
+            formatterVoid = "   ";
+            formatterSpace = "  ";
+        } else if (size > MAX_SIZE_MEDIUM && size <= MAX_SIZE_LARGE) {
+            formatterCol = " %s |";
+            formatterRow = "---+";
+            formatterVoid = "  ";
+            formatterSpace = " ";
         }
     }
 }
