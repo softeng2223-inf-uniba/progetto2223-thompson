e4dcad8529572326880a4d9d655af8964a1b4be8
NicolaM99 <n.mastromarino5@studenti.uniba.it>
2023-06-12 18:50:52 +0200

    Agg. test randomDirection() per dir. non null.

--- src/test/java/it/uniba/app/ship/DirectionTest.java
+++ src/test/java/it/uniba/app/ship/DirectionTest.java
@@ -1,5 +1,21 @@
 package it.uniba.app.ship;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the Direction enum.
+ */
 public class DirectionTest {
+    /**
+     * Tests the randomDirection method to ensure the direction is not null.
+     */
+    @Test
+    void testRandomDirection() {
+        Direction direction = Direction.randomDirection();
+        assertNotNull(direction, "The direction must be not null");
+    }
     
 }
