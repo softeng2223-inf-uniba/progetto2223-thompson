5e88039463777fd2428cf032cc51f37bf090a99c
NicolaM99 <n.mastromarino5@studenti.uniba.it>
2023-06-12 19:59:15 +0200

    Agg. test per getCurrentTries per diff. corrente

--- src/test/java/it/uniba/app/type/DifficultyTest.java
+++ src/test/java/it/uniba/app/type/DifficultyTest.java
@@ -1,5 +1,25 @@
 package it.uniba.app.type;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the Difficulty class.
+ */
 public class DifficultyTest {
-    
+    private static final int TRIES = 50;
+    private static final int SET_TRIES = 8;
+    private static final int HARD_TRIES = 10;
+
+    /**
+     * Tests the getCurrentTries method to ensure it returns the correct number of
+     * current tries for the current difficulty.
+     */
+    @Test
+    void testGetCurrentTries() {
+        Difficulty.setDifficulty(Difficulty.EASY);
+        assertEquals(0, Difficulty.getCurrentTries());
+    }
 }
