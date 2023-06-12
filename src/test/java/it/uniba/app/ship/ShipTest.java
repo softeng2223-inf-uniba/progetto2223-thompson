466a61c376eb83f0714da08275c6a494eb180dac
NicolaM99 <n.mastromarino5@studenti.uniba.it>
2023-06-12 19:07:10 +0200

    Aggiunto test per getSize dim. corretta della nave

--- src/test/java/it/uniba/app/ship/ShipTest.java
+++ src/test/java/it/uniba/app/ship/ShipTest.java
@@ -1,5 +1,22 @@
 package it.uniba.app.ship;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the Ship enum.
+ */
 public class ShipTest {
-    
+    private static final Ship SHIP = Ship.CACCIATORPEDINIERE;
+    private static final int SIZE = 2;
+    private static final int N_SHIPS = 4;
+
+    /**
+     * Tests the getSize method to ensure it returns the correct size of the ship.
+     */
+    @Test
+    void testGetSize() {
+        assertEquals(SIZE, SHIP.getSize());
+    }
 }
